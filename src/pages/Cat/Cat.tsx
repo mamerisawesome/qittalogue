528170082817f06e75257a37e1c78b7773ecdf7a
mamerisawesome <atmalmer23@gmail.com>
2023-03-12 06:24:13 +0800

    feat: Initialize cat profile information page

--- src/pages/Cat/Cat.tsx
+++ src/pages/Cat/Cat.tsx
@@ -1,13 +1,53 @@
 import { useMemo } from 'react';
-import { Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { useGetCatById } from '../../api-query/CatQuery';
+import BackButton from '../../components/BackButton';
+import { COLOR, SIZE } from '../../constants/styles';
 import useIsLoading from '../../hooks/useIsLoading';
 import { ROUTES } from '../../types';
 
-const Container = styled.div``;
+const Profile = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  display: grid;
+  grid-template-columns: 50% 50%;
+  height: 100vh;
+`;
+
+const CatImage = styled(Image)`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+  place-self: end;
+`;
+
+const Information = styled.div`
+  display: flex;
+  align-self: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+
+  padding: ${SIZE.size64};
+
+  text-align: left;
+
+  background-color: ${COLOR.backdrop};
+`;
+
+const InfoItems = styled.div`
+  display: grid;
+  grid-template-columns: 15% 1fr;
+
+  & > * {
+    padding: ${SIZE.size8} ${SIZE.size0};
+  }
+`;
 
 const Cat = () => {
   const { id: catId } = useParams();
@@ -38,20 +78,21 @@ const Cat = () => {
   }
 
   return (
-    <Container>
-      <span>Cat</span>
+    <div>
+      <BackButton href={`${ROUTES.cat}?breed=${catInfo.breedId}`} />
 
-      <Button as="a" href={`${ROUTES.cat}?breed=${catInfo.breedId}`}>Go back</Button>
-
-      <div>
-        <img src={catInfo.url} />
-        <span>{catInfo.breed}</span>
-        <span>{catInfo.origin}</span>
-        <span>{catInfo.temperament}</span>
-        <span>{catInfo.description}</span>
-      </div>
-      <div>{JSON.stringify(catInfo)}</div>
-    </Container>
+      <Profile>
+        <CatImage src={catInfo.url} />
+        <Information>
+          <InfoItems>
+            <h1>{catInfo.breed}</h1><span />
+            <b>Origin</b> <span>{catInfo.origin}</span>
+            <b>Temperament</b> <span>{catInfo.temperament}</span>
+            <b>Description</b> <span>{catInfo.description}</span>
+          </InfoItems>
+        </Information>
+      </Profile>
+    </div>
   );
 };
 
