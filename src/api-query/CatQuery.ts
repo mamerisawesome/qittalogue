64c449e4c0a8ce9cef8c73ddfafc74bfff560834
mamerisawesome <atmalmer23@gmail.com>
2023-03-12 04:31:40 +0800

    feat: Expand breed query to allow for search queries

--- src/api-query/CatQuery.ts
+++ src/api-query/CatQuery.ts
@@ -1,11 +1,20 @@
 import { useQuery } from 'react-query';
 
 import * as CatApi from '../api/CatApi';
+import { Breed } from '../types';
 
 const QUERY_KEYS = {
   getAllBreed: 'breeds',
 };
 
-export const useGetAllBreed = () => (
-  useQuery([QUERY_KEYS.getAllBreed], CatApi.getBreeds)
+export const useGetAllBreed = (searchQuery: string = '') => (
+  useQuery([QUERY_KEYS.getAllBreed], CatApi.getBreeds, {
+    select: (data: Breed[]) => data
+      .filter(({ name }) => name.includes(searchQuery))
+      .map((opt) => ({
+        id: opt.id,
+        name: opt.name,
+        description: opt.description,
+      })),
+  })
 );
